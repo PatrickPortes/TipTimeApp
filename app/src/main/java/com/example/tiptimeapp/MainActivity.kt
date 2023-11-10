0aede1e8f295689ee47b7c45737ab0e4036ecbe2
Patrick Portes <89034086+PatrickPortes@users.noreply.github.com>
2023-11-10 12:44:35 -0300

    Calculate Tip Result Function
    Validating editText Cost Field, developing calculateTipResult function (using radioGroup, and seeing if each of radioButtons are checked and doing the tip calculation).

--- app/src/main/java/com/example/tiptimeapp/MainActivity.kt
+++ app/src/main/java/com/example/tiptimeapp/MainActivity.kt
@@ -1,11 +1,15 @@
 package com.example.tiptimeapp
 
 import android.R
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.RadioButton
 import android.widget.RadioGroup
 import android.widget.Switch
+import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import com.example.tiptimeapp.databinding.ActivityMainBinding
@@ -18,9 +22,15 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var toolbar: Toolbar
     private lateinit var etCost: EditText
-    private lateinit var radioSelected: RadioGroup
+    private lateinit var radioGroup: RadioGroup
+    private lateinit var radioButtonOne: RadioButton
+    private lateinit var radioButtonTwo: RadioButton
+    private lateinit var radioButtonThree: RadioButton
+
+    @SuppressLint("UseSwitchCompatOrMaterialCode")
     private lateinit var switch: Switch
     private lateinit var btCalculate: Button
+    private lateinit var txtResult: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -39,15 +49,75 @@ class MainActivity : AppCompatActivity() {
 
         //Calculate
         btCalculate.setOnClickListener {
-            TODO()
+
+            val tip = etCost.text.toString()
+
+            validateFields(tip)
         }
     }
 
     private fun startingComponents() {
         toolbar = binding.toolbar
         etCost = binding.editTextCost
-        radioSelected = binding.radioGroup
+
+        radioGroup = binding.radioGroup
+        radioButtonOne = binding.radioOne
+        radioButtonTwo = binding.radioTwo
+        radioButtonThree = binding.radioThree
+
         switch = binding.switch1
-        btCalculate  = binding.buttonCalculate
+
+        btCalculate = binding.buttonCalculate
+
+        txtResult = binding.textViewResult
+    }
+
+    private fun validateFields(tip: String) {
+
+        if (tip.isEmpty() || tip.equals("")) {
+            Toast.makeText(this, "Please, Add a Cost of Service!!!", Toast.LENGTH_SHORT).show()
+        } else {
+            calculateTipResult(tip)
+        }
+    }
+
+    private fun calculateTipResult(tip: String) {
+
+        val resultTip = tip.toDouble()
+
+        if (radioButtonOne.isChecked) {
+
+            //Toast.makeText(this, radioButtonOne.text.toString(), Toast.LENGTH_SHORT).show()
+            val tip20 = resultTip * 0.20
+            validateSwitch()
+            txtResult.text = "Tip: $ ${tip20}"
+
+        } else if (radioButtonTwo.isChecked) {
+
+            //Toast.makeText(this, radioButtonTwo.text.toString(), Toast.LENGTH_SHORT).show()
+            val tip18 = resultTip * 0.18
+            validateSwitch()
+            txtResult.text = "Tip: $ ${tip18}"
+
+        } else if (radioButtonThree.isChecked) {
+
+            //Toast.makeText(this, radioButtonThree.text.toString(), Toast.LENGTH_SHORT).show()
+            val tip15 = resultTip * 0.15
+            validateSwitch()
+            txtResult.text = "Tip: $ ${tip15}"
+
+        } else {
+            Toast.makeText(this, "Please, Select How was the Service!!!", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun validateSwitch() {
+
+        if (switch.isChecked) {
+            Toast.makeText(this, "Round up Tip!!!", Toast.LENGTH_SHORT).show()
+            //Round Up Tip:
+            TODO()
+        }
+
     }
 }
